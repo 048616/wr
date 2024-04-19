ae174b17c1c5f871c510942386617c94142872de
Alessandro <alessandro.mariani@dontouch.ch>
2024-04-19 16:42:08 +0200

    wizard

--- components/wizard/WizardContainer.vue
+++ components/wizard/WizardContainer.vue
@@ -13,21 +13,23 @@
 </template>
 
 <script lang="ts" setup>
+
 import { useModalStore } from '~~/store/modalStore'
 
 const modal = useModalStore()
 
-const getComponent = (name: string): any => {
-  let c = null
+const getComponent = (name: string): string  => {
+  let c: string = ""
+  
   switch (name) {
     case 'WizardBook':
-      c = resolveComponent('WizardBook')
+      c = resolveComponent('WizardBook') as string
       break
     case 'WizardUserInfo':
-      c = resolveComponent('WizardUserInfo')
+      c = resolveComponent('WizardUserInfo') as string
       break
     case 'WizardPayment':
-      c = resolveComponent('WizardPayment')
+      c = resolveComponent('WizardPayment') as string
       break
   }
 
